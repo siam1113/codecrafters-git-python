92cdbd5a1b9405eb7633abc70a934e4438e998f9
Siam Hasan <hasan.2017.siam@gmail.com>
2023-10-11 20:49:25 +0600

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 import sys
 import os
+import zlib
+import typing
 
+
+def init():
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/master\n")
+    print("Initialized git directory")
+
+
+def cat_file(option: str, blob_sha: str) -> str:
+    if option == "-p":
+        return zlib.decompress(open(f".git/objects/{blob_sha[0:2]}/{blob_sha[2:]}").read()).decode()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,12 +25,9 @@ def main():
     
     command = sys.argv[1]
     if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/master\n")
-        print("Initialized git directory")
+        init()
+    elif command == "cat-file":
+        print(cat_file(sys.argv[2], sys.argv[3]))
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
