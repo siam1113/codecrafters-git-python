035c6a8c1c6947c1a142fdd781eb732b26c1309e
Siam Hasan <hasan.2017.siam@gmail.com>
2023-10-11 20:57:53 +0600

    fix 2nd stage problems

--- app/main.py
+++ app/main.py
@@ -17,17 +17,22 @@ def cat_file(option: str, blob_sha: str) -> str:
     if option == "-p":
         return zlib.decompress(open(f".git/objects/{blob_sha[0:2]}/{blob_sha[2:]}").read()).decode()
 
+def print_blob(blob_sha: str) -> None:
+    path = f".git/objects/{blob_sha[0:2]}/{blob_sha[2:]}"
+    with open(path, "rb") as f:
+        file = zlib.decompress(f.read())
+        _, content = file.split("\x00")
+        print(content.decode())
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
     
     command = sys.argv[1]
     if command == "init":
         init()
     elif command == "cat-file":
-        print(cat_file(sys.argv[2], sys.argv[3]))
+        cat_file(sys.argv[2], sys.argv[3])
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
